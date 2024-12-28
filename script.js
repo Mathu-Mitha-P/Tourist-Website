7d464f96d6b3e1742b6dc82523929614b47e3e95
Mathu-Mitha-P <pjmathumithait@gmail.com>
2024-12-28 18:45:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-alert("Have a nice day!");  
-function login()
-{
-        var userchoice=confirm("Do you want to login?");         
-        if(userchoice==true)
-        {
-                var username=prompt(" What is your name? ");
-                alert("Great! Login successfully");
+let timeoutInitialized = false;
+
+if (!timeoutInitialized) {
+    timeoutInitialized = true;
+
+    var a = setTimeout(login, 2000); // Call login after 2 seconds
+
+    function login() {
+        var userChoice = confirm("Do you want to login?");
+        if (userChoice) {
+            var username = prompt("What is your name?");
+            window.location.href = "Login.html";
+            alert("Great! Start Your Login");
+        } else {
+            alert("Login Cancelled");
         }
-        else
-        {
-                alert("Login Cancelled");
-        }
-}   
+    }
+} 
+//scroll
+
+document.getElementById('homepage').addEventListener('click', function (e) {
+    e.preventDefault(); // Prevent default anchor click behavior
+    document.getElementById('placesToVisit').scrollIntoView({ behavior: 'smooth' });
+});
